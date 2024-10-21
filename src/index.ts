d7d2836e516c05df2e42fb9d0234932c1c2b6c6f
Matheus Valentim <meduardo.valentim@gmail.com>
2024-10-20 23:41:24 -0300

    feat: create index for server

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import { clientRoutes } from './routes/clientRoutes';
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/clients', clientRoutes);
+//app.use('/properties', propertyRoutes);
+//app.use('/deals', dealRoutes);
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:5432');
+});
